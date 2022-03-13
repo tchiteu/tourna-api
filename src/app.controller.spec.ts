3116d683f782bc1177a327b1435632bafa2fbde9
tchiteu <matheussantos@estudante.sc.senai.br>
2022-03-13 20:48:53 -0300

    Starting the project!

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Tourna is coming!"', () => {
+      expect(appController.getHello()).toBe('Tourna is coming!');
+    });
+  });
+});
